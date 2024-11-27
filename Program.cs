e8aa456515886496a1d9472247db6b33f0a4af48
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-11-26 22:55:22 -0600

    [1.08] Use appsettings.json for connection string

--- Program.cs
+++ Program.cs
@@ -1,7 +1,49 @@
 ﻿using NLog;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using NorthwindConsole.Model;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
 logger.Info("Program started");
 Console.WriteLine("Hello World!");
 logger.Info("Program ended");
+
+
+
+
+do
+{
+  Console.WriteLine("1) Display categories");
+  Console.WriteLine("2) Add category");
+  Console.WriteLine("Enter to quit");
+  string? choice = Console.ReadLine();
+  Console.Clear();
+  logger.Info("Option {choice} selected", choice);
+  if (choice == "1")
+  {
+    var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+    var config = configuration.Build();
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryName);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{query.Count()} records returned");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    foreach (var item in query)
+    {
+      Console.WriteLine($"{item.CategoryName} - {item.Description}");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+  }
+  else if (choice == "2")
+  {
+    // Add category
+  }
+  else if (String.IsNullOrEmpty(choice))
+  {
+    break;
+  }
+  Console.WriteLine();
+} while (true);
