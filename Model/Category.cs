bce361571d5ecdc5f68ac555e3d6829dad417214
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-12-07 23:59:59 -0600

    [1.12] Require CategoryName using annotations

--- Model/Category.cs
+++ Model/Category.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NorthwindConsole.Model;
 
 public partial class Category
 {
     public int CategoryId { get; set; }
-
+       [Required]
     public string CategoryName { get; set; } = null!;
 
     public string? Description { get; set; }
